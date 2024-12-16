33047bb08cb037a8348e799ec0092376bcd532be
GuticaStefan <stefan.gutica@gmail.com>
2024-12-17 01:35:44 +0200

    add tx processor

--- apps/offline-jobs/src/crons/crons.module.ts
+++ apps/offline-jobs/src/crons/crons.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EachClusterModule } from './each-cluster/each-cluster.module';
 import { GlobalModule } from './global/global.module';
+import { TransactionProcessorModule } from './transaction-processor/transaction-processor.module';
 
 @Module({
   imports: [
     EachClusterModule,
     GlobalModule,
+    TransactionProcessorModule,
   ],
 })
 export class CronsModule { }
