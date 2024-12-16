ad6cbb42a4c573d2775d018ff45e492fd8c874ed
GuticaStefan <stefan.gutica@gmail.com>
2024-12-16 21:57:19 +0200

    qr code base64

--- libs/core/src/modules/transaction-generator/transaction-generator.service.ts
+++ libs/core/src/modules/transaction-generator/transaction-generator.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import QRCode from 'qrcode';
+// import { XBounty } from './xBounty';
 
 @Injectable()
 export class TransactionGeneratorService {
-  constructor() { }
+  // private readonly xBountyContractService: XBounty;
+  private readonly scAddress: string;
+  constructor() {
+    // this.xBountyContractService = new XBounty();
+  }
 
-  async execute(): Promise<void> {
-    console.log('Generating transaction...');
+  async execute(amount: string, issueId: number, repoUrl: string) {
+    const repoUrlHex = Buffer.from(repoUrl, 'utf8').toString('hex');
+    const issueIdHex = issueId.toString(16);
+    const bip21String = `elrond:${this.scAddress}?amount=${parseFloat(amount)}&gasLimit=4000000&message=fund@${repoUrlHex}@${issueIdHex}`;
+    const qrCode = await this.generateQRCodeFromBIP21(bip21String);
+    return qrCode;
+  }
+
+  async generateQRCodeFromBIP21(bip21String: string): Promise<string | undefined> {
+    try {
+      const qrDataURL = await QRCode.toDataURL(bip21String);
+      console.log('QR Code Data URL:', qrDataURL);
+      return qrDataURL;
+    } catch (error) {
+      console.error('Error generating QR code', error);
+      return undefined;
+    }
   }
 }
